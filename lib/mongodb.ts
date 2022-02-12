3b53787f27ea35de1a8690f51b1e550b8eb8216a
UnusualAbsurd <nottherealabsurdchad@gmail.com>
2022-02-12 12:15:35 +0700

    Fix mongodb github test error ⛳

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -9,10 +9,6 @@ const options = {
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env");
-}
-
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
